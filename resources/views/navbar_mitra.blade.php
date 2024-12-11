c82c595a6b1f08913482135d363586575404561b
AndikaRT421 <trahmanandika314@gmail.com>
2024-12-11 20:20:44 +0700

    feat: fix minor navbar

--- resources/views/navbar_mitra.blade.php
+++ resources/views/navbar_mitra.blade.php
@@ -20,10 +20,6 @@
                 <a href="{{ route('mitra.dashboard') }}" class="nav-link text-gray-800 font-semibold hover:text-black transition hover:underline">Beranda</a>
                 <a href="{{ route('mitra.add_product') }}" class="nav-link text-gray-800 font-semibold hover:text-black transition hover:underline">Tambah Produk</a>
                 <a href="{{ route('mitra.check_store') }}" class="nav-link text-gray-800 font-semibold hover:text-black transition hover:underline">Cek Toko</a>
-<<<<<<< HEAD
-=======
-                {{-- <a href="#" class="nav-link text-gray-800 font-semibold hover:text-black transition hover:underline">Chat Pembeli</a> --}}
->>>>>>> fe7cfbbab48840a308c6792753ea7492c41b76f3
             </div>
         </div>
 
